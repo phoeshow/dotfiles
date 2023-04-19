0290f98ce679c010ae78c138e51ef6c51c5dd090
phoeshow.me <phoeshow.me@gmail.com>
2023-04-19 15:28:27 +0800

    lazy git terminal window resize

--- nvim/.config/nvim/lua/custom/lazygit.lua
+++ nvim/.config/nvim/lua/custom/lazygit.lua
@@ -1,8 +1,11 @@
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  direction = 'float'
+})
 
-function _lazygit_toggle()
-  lazygit:toggle()
-end
+function _lazygit_toggle() lazygit:toggle() end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>",
+  { noremap = true, silent = true })
