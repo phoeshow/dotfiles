03c78c31c54e3dd06f96f1073887c79c472f7eea
phoeshow.me <phoeshow.me@gmail.com>
2023-05-07 18:07:08 +0800

    update

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ lualine.setup({
     icons_enabled = true,
     theme = "catppuccin",
     component_separators = { left = "|", right = "|" },
-    section_separators = { left = "", right = "" },
+    section_separators = { left = "█", right = "█" },
     always_divide_middle = true,
     globalstatus = false,
   },
